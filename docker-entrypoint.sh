59d65b22075f9ff47016f9839445613ec1e171db
Aran30 <aaron30@live.be>
2021-08-09 19:49:51 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ fi
 NODE_ID_SEED=${NODE_ID_SEED:-$RANDOM}
 
 # set some helpful variables
-export SERVICE_PROPERTY_FILE='/etc/i5.las2peer.services.contactService.ContactService.properties'
+export SERVICE_PROPERTY_FILE='etc/i5.las2peer.services.contactService.ContactService.properties'
 export SERVICE_PASSPHRASE_FILE='etc/startup/passphrases.txt'
 export WEB_CONNECTOR_PROPERTY_FILE='etc/i5.las2peer.connectors.webConnector.WebConnector.properties'
 export SERVICE_VERSION=$(awk -F "=" '/service.version/ {print $2}' gradle.properties )
@@ -24,7 +24,7 @@ echo ${SERVICE}
 echo ${CONTACT_STORER_NAME}
 echo ${CONTACT_STORER_PW}
 
-cd etc/ && ls && mkdir startup && ls && cd startup/ && touch passphrases.txt && ls && echo "agent-user-contact.xml;${CONTACT_STORER_PW}"  >> passphrases.txt
+echo "agent-user-contact.xml;${CONTACT_STORER_PW}"  >> etc/startup/passphrases.txt
 # set defaults for optional service parameters
 [[ -z "${SERVICE_PASSPHRASE}" ]] && export SERVICE_PASSPHRASE='contacts'
 
