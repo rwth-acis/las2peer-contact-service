b40055aedc80b641bade663433005eead6b781ce
Aran30 <aaron30@live.be>
2021-08-09 20:33:59 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -101,5 +101,6 @@ if [[ -z "${@}" ]]; then
         exec ${LAUNCH_COMMAND} --observer --node-id-seed $NODE_ID_SEED startService\("'""${SERVICE}""'", "'""${SERVICE_PASSPHRASE}""'"\) startWebConnector
     fi
 else
+    echo ok
     exec ${LAUNCH_COMMAND} uploadStartupDirectory ${@}
 fi
